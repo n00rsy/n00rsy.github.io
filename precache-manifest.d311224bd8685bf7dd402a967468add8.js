e75b5e12a37f41474e7b34198de6ffac89190a4f
Noor Syed <noorsyed1a@gmail.com>
2020-06-21 00:20:14 -0400

    Updates

---
+++ precache-manifest.d311224bd8685bf7dd402a967468add8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad743e08f18cbfa7604074e819f1122c",
+    "url": "/site/index.html"
+  },
+  {
+    "revision": "50bb0b0b4aa067971fec",
+    "url": "/site/static/css/main.a3f70fa4.chunk.css"
+  },
+  {
+    "revision": "115b7f7713534a008e73",
+    "url": "/site/static/js/2.ca071efc.chunk.js"
+  },
+  {
+    "revision": "b23a8a042e51dd51bbd0294055a0b702",
+    "url": "/site/static/js/2.ca071efc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50bb0b0b4aa067971fec",
+    "url": "/site/static/js/main.c8da871c.chunk.js"
+  },
+  {
+    "revision": "a6f832edb43cb75d95e7",
+    "url": "/site/static/js/runtime-main.6815615a.js"
+  }
+]);
